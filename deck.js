473ac53caba0065393f22fd3d195de6964d1936f
jorgemarrero <jorge@secret-source.eu>
2018-10-05 14:44:49 +0100

    Add colored tag

--- deck.js
+++ deck.js
@@ -4,5 +4,7 @@ import graphql from "./decks/3-graphql.mdx";
 import workshop from "./decks/4-workshop.mdx";
 import end from "./decks/5-end.mdx";
 
+import "style-loader!css-loader!./styles.css";
+
 export { default as theme } from "./src/theme";
 export default [...intro, ...rest, ...graphql, ...workshop, ...end];
