cbe2327eb3f55d0db46d3015a25d0d0bc2c8dca3
jorgemarrero <jorgems93@gmail.com>
2018-10-05 10:42:34 +0100

    refactor: Use multiple mdx files

---
+++ deck.js
@@ -0,0 +1,8 @@
+import intro from "./decks/1-intro.mdx";
+import rest from "./decks/2-rest.mdx";
+import graphql from "./decks/3-graphql.mdx";
+import workshop from "./decks/4-workshop.mdx";
+import end from "./decks/5-end.mdx";
+
+export { default as theme } from "./src/theme";
+export default [...intro, ...rest, ...graphql, ...workshop, ...end];
